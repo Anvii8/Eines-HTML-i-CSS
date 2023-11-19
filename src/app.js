d44b95e4a1bf5c3f8152ed8b1605606687dad6b4
AlbertAndresVidueira <aandresvi@uoc.edu>
2023-11-19 10:37:32 +0100

    interacción del header.html con fichero app.js mejorada

--- src/app.js
+++ src/app.js
@@ -1,26 +1,26 @@
-let urlCompleta = document.URL;
+const urlCompleta = document.URL;
 const portada = document.getElementById('portada');
 const presentacion = document.getElementById('presentacion');
 const categoria = document.getElementById('categoria');
 const enlaces = document.getElementById('enlaces');
 
-let partesURL = urlCompleta.split('/');
-let nombreArchivo = partesURL[partesURL.length - 1];
+const partesURL = urlCompleta.split('/');
+const nombreArchivo = partesURL[partesURL.length - 1];
 
 indicarPagina(nombreArchivo);
 
 
 function indicarPagina(nombreArchivo) {
-    if (nombreArchivo === "index.html" || nombreArchivo === ""){
+    if (nombreArchivo.includes("index") || nombreArchivo === ""){
         portada.style.color = "rgb(254, 105, 106)";
     }
-    else if (nombreArchivo === "presentacio.html"){
+    else if (nombreArchivo.includes("presentacio")){
         presentacion.style.color = "rgb(254, 105, 106)";
     }
-    else if (nombreArchivo === "categoria.html"){
+    else if (nombreArchivo.includes("categoria")){
         categoria.style.color = "rgb(254, 105, 106)";
     }
-    else if (nombreArchivo === "enlaces.html"){
+    else if (nombreArchivo.includes("enlaces")){
         enlaces.style.color = "rgb(254, 105, 106)";
     }
 }
