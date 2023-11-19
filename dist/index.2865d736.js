ba119955b30401503cdb061781843413be64153f
AlbertAndresVidueira <aandresvi@uoc.edu>
2023-11-19 10:28:15 +0100

    interacción del header.html con fichero app.js

---
+++ dist/index.2865d736.js
@@ -0,0 +1,3 @@
+var e;let t=document.URL;const l=document.getElementById("portada"),o=document.getElementById("presentacion"),n=document.getElementById("categoria"),c=document.getElementById("enlaces");let r=t.split("/");"index.html"===(e=r[r.length-1])||""===e?l.style.color="rgb(254, 105, 106)":"presentacio.html"===e?o.style.color="rgb(254, 105, 106)":"categoria.html"===e?n.style.color="rgb(254, 105, 106)":"enlaces.html"===e&&(c.style.color="rgb(254, 105, 106)");//# sourceMappingURL=index.2865d736.js.map
+
+//# sourceMappingURL=index.2865d736.js.map
--- src/app.js
+++ src/app.js
@@ -1,2 +1,26 @@
-const name = 'world';
-console.log(`Hello ${name}`);
+let urlCompleta = document.URL;
+const portada = document.getElementById('portada');
+const presentacion = document.getElementById('presentacion');
+const categoria = document.getElementById('categoria');
+const enlaces = document.getElementById('enlaces');
+
+let partesURL = urlCompleta.split('/');
+let nombreArchivo = partesURL[partesURL.length - 1];
+
+indicarPagina(nombreArchivo);
+
+
+function indicarPagina(nombreArchivo) {
+    if (nombreArchivo === "index.html" || nombreArchivo === ""){
+        portada.style.color = "rgb(254, 105, 106)";
+    }
+    else if (nombreArchivo === "presentacio.html"){
+        presentacion.style.color = "rgb(254, 105, 106)";
+    }
+    else if (nombreArchivo === "categoria.html"){
+        categoria.style.color = "rgb(254, 105, 106)";
+    }
+    else if (nombreArchivo === "enlaces.html"){
+        enlaces.style.color = "rgb(254, 105, 106)";
+    }
+}
