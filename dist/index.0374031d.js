573d90d18d34b181215f5f30ffd651fb587117b9
AlbertAndresVidueira <aandresvi@uoc.edu>
2023-12-07 12:48:16 +0100

    carpetas dist y parcel eliminadas del repositorio

--- dist/index.0374031d.js
+++
@@ -1,3 +0,0 @@
-const e=document.URL,t=document.getElementById("portada"),n=document.getElementById("presentacion"),c=document.getElementById("categoria"),l=document.getElementById("enlaces"),o=e.split("/"),d=o[o.length-1];d.includes("index")||""===d?t.style.color="rgb(254, 105, 106)":d.includes("presentacio")?n.style.color="rgb(254, 105, 106)":d.includes("categoria")?c.style.color="rgb(254, 105, 106)":d.includes("enlaces")&&(l.style.color="rgb(254, 105, 106)");//# sourceMappingURL=index.0374031d.js.map
-
-//# sourceMappingURL=index.0374031d.js.map
---
+++ dist/index.816e7b21.js
@@ -0,0 +1,16 @@
+const urlCompleta = document.URL;
+const portada = document.getElementById("portada");
+const presentacion = document.getElementById("presentacion");
+const categoria = document.getElementById("categoria");
+const enlaces = document.getElementById("enlaces");
+const partesURL = urlCompleta.split("/");
+const nombreArchivo = partesURL[partesURL.length - 1];
+indicarPagina(nombreArchivo);
+function indicarPagina(nombreArchivo) {
+    if (nombreArchivo.includes("index") || nombreArchivo === "") portada.style.color = "rgb(254, 105, 106)";
+    else if (nombreArchivo.includes("presentacio")) presentacion.style.color = "rgb(254, 105, 106)";
+    else if (nombreArchivo.includes("categoria")) categoria.style.color = "rgb(254, 105, 106)";
+    else if (nombreArchivo.includes("enlaces")) enlaces.style.color = "rgb(254, 105, 106)";
+}
+
+//# sourceMappingURL=index.816e7b21.js.map
